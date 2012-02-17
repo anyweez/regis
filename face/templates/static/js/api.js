36c511e7ebbfed34e094994bea1e3b0e37a26a1c
Christopher Cartland <cartland@cartland-vbox.(none)>
2012-02-16 18:04:45 -0800

    api.js is updated and questions.js is removed

--- face/templates/static/js/api.js
+++ face/templates/static/js/api.js
@@ -10,5 +10,13 @@ var questions = new function() {
        $.getJSON('/api/questions/list', callback);};
 }
 
+function view_question_handler(data) {
+  $('question_body').html(data.html);
+}
 
+function list_questions_handler(data) {
+  for (var i = 0; i < data.items.length; i++) {
+    $('question_body').append(data.items[i].html);
+  }
+}
 
